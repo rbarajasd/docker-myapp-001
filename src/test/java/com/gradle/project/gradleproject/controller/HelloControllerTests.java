7cdcaf761650d790efe9c659d159b8b2bb4272ec
Rafael J Barajas <rafaeljbarajas@ibm.com>
2021-06-11 01:00:26 -0500

    adding travis config

--- src/test/java/com/gradle/project/gradleproject/controller/HelloControllerTests.java
+++ src/test/java/com/gradle/project/gradleproject/controller/HelloControllerTests.java
@@ -3,7 +3,6 @@ package com.gradle.project.gradleproject.controller;
 import com.gradle.project.gradleproject.constants.Constants;
 import com.gradle.project.gradleproject.model.VisitsModel;
 import com.gradle.project.gradleproject.repository.VisitsRepository;
-import com.gradle.project.gradleproject.service.VisitsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +14,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
-import static reactor.core.publisher.Mono.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerTests {
